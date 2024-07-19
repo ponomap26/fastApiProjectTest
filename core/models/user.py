b4946e657acd3c739cfda78dd30c19e87f172398
ponomap26 <ponomap26@yandex.ru>
2024-07-19 21:10:21 +0400

    create User model

---
+++ core/models/user.py
@@ -0,0 +1,10 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class User(Base, SQLAlchemyBaseUserTable[int]):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    pass
